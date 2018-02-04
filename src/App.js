e61138750892178199121953759f589d50472735
Mark Bryan <mab539@cornell.edu>
2018-02-04 13:24:36 -0500

    add date range picker

--- src/App.js
+++ src/App.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Navbar,
+  NavbarGroup,
+  NavbarHeading,
+  Button,
+} from '@blueprintjs/core';
+import { DateRangePicker } from '@blueprintjs/datetime';
+import {
+  ButtonGroup
+} from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: null,
+      endDate: null,
+      vocalization: 'Select dates and run vocalization to see the output here.'
+    }
+  }
+
+  handleTableSelection = (e) => {
+    console.log(e);
+  }
+
+  handleDateChange = (selectedDates) => {
+    this.setState({
+        startDate: selectedDates[0],
+        endDate: selectedDates[1]
+    })
+  }
+
+  getVocalization = (e) => {
+    console.log('Fetching vocalization...');
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar>
+          <NavbarGroup>
+              <NavbarHeading>Cicero - Time Series</NavbarHeading>
+          </NavbarGroup>
+        </Navbar>
+
+        <div className="container">
+          <ButtonGroup>
+            <Button onClick={this.handleTableSelection}>bitstampusd</Button>
+            <Button onClick={this.handleTableSelection}>coinbaseusd</Button>
+            <Button onClick={this.handleTableSelection}>sensor</Button>
+          </ButtonGroup>
+
+          <div className="row">
+            <DateRangePicker
+              value={[this.state.startDate, this.state.endDate]}
+              onChange={this.handleDateChange}
+            />
+          </div>
+
+          <div className="row">
+            <Button onClick={this.getVocalization}>Get Vocalization</Button>
+          </div>
+
+          <div>
+            {this.state.vocalization}
+          </div>
+        </div>
       </div>
     );
   }
