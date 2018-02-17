9eac3d49e3f0497820f7de5ab86573dd347a6e88
Mark Bryan <mab539@cornell.edu>
2018-02-17 15:07:01 -0600

    adjust intro text

--- src/Intro.js
+++ src/Intro.js
@@ -5,19 +5,16 @@ class Intro extends Component {
     return (
       <div className="intro" style={{ margin: "20px", align: "left" }}>
         <h1 className="display-4">Welcome to Cicero DB</h1>
-        <p className="lead">
-          We present a unique way to interact with data: through a voice interface.
-        </p>
         <p>
           At the bottom of this page, there is a timeline representing the price of a financial asset
           for when it traded from December 2011 to January 2018. Use the left and right markers on
           the timeline to select a time range. Then, click the 'Get Vocalization' button to generate
-          and hear a voice description of how the price of the asset has changed during the selected time range.
+          and hear a voice description of how the price of the asset has evolved over the selected time range.
         </p>
         <p>
-          We have provided two methods for voice generation, which you can switch between below.
-          The first method uses sampling to quickly generate high quality voice descriptions of the data.
-          The second method processes the entire data for a time range to create the best description of the data.
+          You can select between two voice generation methods below.
+          The first method uses sampling to quickly generate approximate voice descriptions of the data.
+          The second method processes the entire data for a time range to create the best possible description of the data.
           However, this second method may take much longer than the sampling method due to the amount of data it has
           to process. You can freely switch between the two methods as you explore the data.
         </p>
