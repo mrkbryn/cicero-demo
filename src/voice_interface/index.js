9f2bd26e663b8b62aa798505aa0a2d06fad91284
Mark Bryan <mab539@cornell.edu>
2018-04-16 13:01:41 -0400

    set executingCommand correctly

--- src/voice_interface/index.js
+++ src/voice_interface/index.js
@@ -9,6 +9,7 @@ class VoiceInterface extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      executingCommand: false,
       vocalizationFetch: {
         fetching: false
       }
@@ -17,32 +18,35 @@ class VoiceInterface extends Component {
 
   fetchVocalizationFromBackend = (command, onStartExecutingCommand, onEndExecutingCommand) => {
     onStartExecutingCommand()
-    this.setState({ vocalizationFetch: { fetching: true, command }})
+    this.setState({ executingCommand: true, vocalizationFetch: { fetching: true, command }})
     fetchVocalization(command)
     .then(response => response.json())
     .then(json => {
       if (json.error) {
-        this.setState({ vocalizationFetch: { fetching: false, error: json.message }})
+        this.setState({ executingCommand: false, vocalizationFetch: { fetching: false, error: json.message }})
         onEndExecutingCommand()
       } else {
         this.setState({ vocalizationFetch: { fetching: false, result: json }})
-        var synth = window.speechSynthesis;
-        var voices = synth.getVoices();
-        var voiceOutput = new SpeechSynthesisUtterance(json.vocalization);
+        var synth = window.speechSynthesis
+        synth.cancel()
+        var voices = synth.getVoices()
+        var voiceOutput = new SpeechSynthesisUtterance(json.vocalization)
         for (var i = 0; i < voices.length; i++) {
           if (voices[i].voiceURI === 'Google UK English Female') {
             voiceOutput.voice = voices[i]
-            break;
+            break
           }
         }
         voiceOutput.onend = (event) => {
+          this.setState({ executingCommand: false })
           onEndExecutingCommand()
         }
-        synth.speak(voiceOutput);
+        synth.speak(voiceOutput)
       }
     })
     .catch(error => {
       console.log(error)
+      this.setState({ executingCommand: false })
       onEndExecutingCommand()
     })
   }
@@ -54,7 +58,7 @@ class VoiceInterface extends Component {
         <SuggestedUse />
         <QueryInputComponent
           fetchVocalizationFromBackend={this.fetchVocalizationFromBackend}
-          executingCommand={this.state.vocalizationFetch.fetching}
+          executingCommand={this.state.executingCommand}
         />
         <VocalizationFetch
           vocalizationFetch={this.state.vocalizationFetch}
